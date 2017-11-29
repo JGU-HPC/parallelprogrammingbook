412769d61c64029e9f380bda26c67a396eb801bd
Christian Hundt <christian@iaimz140.Informatik.Uni-Mainz.DE>
2017-11-29 12:54:24 +0100

    MPI Jacobi Iteration

---
+++ chapter9/jacobi_iteration/jacobi_1D_block_simple.cpp
@@ -0,0 +1,202 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+#include "mpi.h"
+
+void readInput(std::string file, int rows, int cols, float *data){
+
+	// Open the file pointer
+	/*FILE* fp = fopen(file.c_str(), "rb");
+
+	// Check if the file exists
+	if(fp == NULL){
+		std::cout << "ERROR: File " << file << " could not be opened" << std::endl;
+		MPI::COMM_WORLD.Abort(1);
+	}
+
+	for(int i=0; i<rows*cols; i++){
+		if(!fscanf(fp, "%f", &data[i])){
+			std::cout << "ERROR: Not enough values in file " << file << std::endl;
+			MPI::COMM_WORLD.Abort(1);
+		}
+	}*/
+
+    // checkerboard
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<cols; j++)
+            data[i*cols+j] = (i/121+j/121) % 2;
+}
+
+void printOutput(std::string file, int rows, int cols, float *data){
+
+	FILE *fp = fopen(file.c_str(), "wb");
+	// Check if the file was opened
+	if(fp == NULL){
+		std::cout << "ERROR: Output file " << file << " could not be opened" << std::endl;
+		MPI::COMM_WORLD.Abort(1);
+	}
+
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++)
+        	fprintf(fp, "%lf ", data[i*cols+j]);
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+}
+
+int main (int argc, char *argv[]){
+	// Initialize MPI
+	MPI::Init(argc,argv);
+
+	// Get the number of processes
+	int numP=MPI::COMM_WORLD.Get_size();
+
+	// Get the ID of the process
+	int myId=MPI::COMM_WORLD.Get_rank();
+
+	if(argc < 6){
+		// Only the first process prints the output message
+		if(!myId){
+			std::cout << "ERROR: The syntax of the program is " << argv[0]
+                                  << " inputFile rows cols outputFile errThreshold"
+			          << std::endl;
+		}
+		MPI::COMM_WORLD.Abort(1);
+	}
+
+	std::string inputFile = argv[1];
+	int rows = atoi(argv[2]);
+	int cols = atoi(argv[3]);
+	std::string outputFile = argv[4];
+	float errThres = atof(argv[5]);
+
+	if((rows < 1) || (cols < 1)){
+		// Only the first process prints the output message
+		if(!myId)
+			std::cout << "ERROR: The number of rows and columns must be higher than 0" << std::endl;
+
+		MPI::COMM_WORLD.Abort(1);
+	}
+
+	if(rows%numP){
+		// Only the first process prints the output message
+		if(!myId)
+			std::cout << "ERROR: The number of rows must be multiple of the number of processes" << std::endl;
+		
+		MPI::COMM_WORLD.Abort(1);
+	}
+
+	float *data;
+
+	// Only one process reads the data from the file and broadcasts the data
+	if(!myId){
+		data = new float[rows*cols];
+		readInput(inputFile, rows, cols, data);
+	}
+
+	// The computation is divided by rows
+	int myRows = rows/numP;
+
+	// Arrays for the chunk of data to work
+	float *myData = new float[myRows*cols];
+	float *buff = new float[myRows*cols];
+
+	MPI::COMM_WORLD.Barrier();
+
+	// Measure the current time
+	double start = MPI::Wtime();
+
+	// Scatter the input matrix
+	MPI::COMM_WORLD.Scatter(data, myRows*cols, MPI::FLOAT, myData, myRows*cols, MPI::FLOAT, 0);
+	memcpy(buff, myData, myRows*cols*sizeof(float));
+
+	float error = errThres+1.0;
+	float myError;
+
+	// Buffers to receive the rows
+	float *prevRow = new float[cols];
+	float *nextRow = new float[cols];
+
+	while(error > errThres){
+		if(myId > 0){
+			// Send the first row to the previous process
+			MPI::COMM_WORLD.Send(myData, cols, MPI::FLOAT, myId-1, 0);
+		}
+
+		if(myId < numP-1){
+			// Receive the next row from the next process
+			MPI::COMM_WORLD.Recv(nextRow, cols, MPI::FLOAT, myId+1, 0);
+
+			// Send the last row to the next process
+			MPI::COMM_WORLD.Send(&myData[(myRows-1)*cols], cols, MPI::FLOAT, myId+1, 0);
+		}
+
+		if(myId > 0){
+			// Receive the previous row from the previous process
+			MPI::COMM_WORLD.Recv(prevRow, cols, MPI::FLOAT, myId-1, 0);
+		}
+
+		// Update the first row
+		if((myId > 0) && (myRows>1)){
+			for(int j=1; j<cols-1; j++){
+				buff[j] = 0.25*(myData[cols+j]+myData[j-1]+myData[j+1]+prevRow[j]);
+			}
+		}
+
+		// Update the main block
+		for(int i=1; i<myRows-1; i++){
+			for(int j=1; j<cols-1; j++){
+				// calculate discrete laplacian by averaging 4-neighbourhood
+				buff[i*cols+j]= 0.25f*(myData[(i+1)*cols+j]+myData[i*cols+j-1]+myData[i*cols+j+1]+myData[(i-1)*cols+j]);
+			}
+		}
+
+		// Update the last row
+		if((myId < numP-1) && (myRows > 1)){
+			for(int j=1; j<cols-1; j++){
+				buff[(myRows-1)*cols+j] = 0.25*(nextRow[j]+myData[(myRows-1)*cols+j-1]+
+						myData[(myRows-1)*cols+j+1]+myData[(myRows-2)*cols+j]);
+			}
+		}
+
+		// Calculate the error of the block
+		myError = 0.0;
+		for(int i=0; i<myRows; i++){
+			for(int j=1; j<cols-1; j++){
+                // determine difference between 'data' and 'buff' and add up error
+                myError += (myData[i*cols+j]-buff[i*cols+j])*(myData[i*cols+j]-buff[i*cols+j]);
+			}
+		}
+
+		memcpy(myData, buff, myRows*cols*sizeof(float));
+
+		// Sum the error of all the processes
+		// The output is stored in the variable 'error' of all processes
+		MPI::COMM_WORLD.Allreduce(&myError, &error, 1, MPI::FLOAT, MPI::SUM);
+	}
+
+	// Only process 0 writes
+	// Gather the final matrix to the memory of process 0
+	MPI::COMM_WORLD.Gather(myData, myRows*cols, MPI::FLOAT, data, myRows*cols, MPI::FLOAT, 0);
+
+	// Measure the current time
+	double end = MPI::Wtime();
+
+	if(!myId){
+    	std::cout << "Time with " << numP << " processes: " << end-start << " seconds" << std::endl;
+    	printOutput(outputFile, rows, cols, data);
+		delete [] data;
+	}
+
+	delete [] myData;
+	delete [] buff;
+	delete [] prevRow;
+	delete [] nextRow;
+
+	// Terminate MPI
+	MPI::Finalize();
+	return 0;
+}
---
+++ chapter9/jacobi_iteration/jacobi_seq.cpp
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <chrono>
+
+void readInput(std::string file, int rows, int cols, float *data){
+
+	// Open the file pointer
+	/*FILE* fp = fopen(file.c_str(), "rb");
+
+	// Check if the file exists
+	if(fp == NULL){
+		std::cout << "ERROR: File " << file << " could not be opened" << std::endl;
+		exit(1);
+	}
+
+	for(int i=0; i<rows*cols; i++){
+		if(!fscanf(fp, "%f", &data[i])){
+			std::cout << "ERROR: Not enough values in file " << file << std::endl;
+			exit(1);
+		}
+	}*/
+
+    // checkerboard
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<cols; j++)
+            data[i*cols+j] = (i/121+j/121) % 2;
+
+    //fclose(fp);
+}
+
+void printOutput(std::string file, int rows, int cols, float *data){
+
+	FILE *fp = fopen(file.c_str(), "wb");
+	// Check if the file was opened
+	if(fp == NULL){
+		std::cout << "ERROR: Output file " << file << " could not be opened" << std::endl;
+		exit(1);
+	}
+
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++)
+        	fprintf(fp, "%lf ", data[i*cols+j]);
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+}
+
+int main (int argc, char *argv[]){
+	if(argc < 6){
+		std::cout << "ERROR: The syntax of the program is ./jacobi-1D inputFile rows cols outputFile errThreshold"
+				<< std::endl;
+		exit(1);
+	}
+
+	std::string inputFile = argv[1];
+	int rows = atoi(argv[2]);
+	int cols = atoi(argv[3]);
+	std::string outputFile = argv[4];
+	float errThres = atof(argv[5]);
+
+	if((rows < 1) || (cols < 1)){
+		std::cout << "ERROR: The number of rows and columns must be higher than 0" << std::endl;
+		exit(1);
+	}
+
+	float *data = new float[rows*cols];
+	float *buff = new float[rows*cols];
+	readInput(inputFile, rows, cols, data);
+	memcpy(buff, data, rows*cols*sizeof(float));
+
+	// Measure the current time
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+
+	float error = errThres + 1.0;
+
+	while(error > errThres){
+		for(int i=1; i<rows-1; i++){
+			for(int j=1; j<cols-1; j++){
+				// calculate discrete laplacian by averaging 4-neighbourhood
+				buff[i*cols+j]= 0.25f*(data[(i+1)*cols+j]+data[i*cols+j-1]+data[i*cols+j+1]+data[(i-1)*cols+j]);
+			}
+		}
+
+		// Determine the error
+		error = 0.0;
+		for(int i=1; i<rows-1; i++){
+			for(int j=1; j<cols-1; j++){
+                // determine difference between 'data' and 'buff' and add up error
+                error += (data[i*cols+j]-buff[i*cols+j])*(data[i*cols+j]-buff[i*cols+j]);
+			}
+		}
+
+		memcpy(data, buff, rows*cols*sizeof(float));
+	}
+
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<float> elapsed_seconds = end-start;
+
+	std::cout << "Sequential Jacobi with dimensions " << rows << "x" << cols << " in " << elapsed_seconds.count()
+			<< " seconds" << std::endl;
+
+	printOutput(outputFile, rows, cols, data);
+
+	delete [] data;
+	delete [] buff;
+
+	return 0;
+}
